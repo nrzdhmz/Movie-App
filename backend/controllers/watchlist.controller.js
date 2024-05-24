a0271f075ebce8bd4dba3268263f8f854487a5f7
moaliyev <mehemmed05.aliyev@gmail.com>
2024-05-24 20:37:28 +0400

    gitignore updated

--- backend/controllers/watchlist.controller.js
+++ backend/controllers/watchlist.controller.js
@@ -1,5 +1,7 @@
+import axios from "axios";
 import prisma from "./../prismaClient/index.js";
 
+// ADD MOVIE TO WATCHLIST
 export const addWatchlistController = async (req, res) => {
   try {
     const { movieId } = req.body;
@@ -13,20 +15,69 @@ export const addWatchlistController = async (req, res) => {
           user: {
             connect: { id: req.user.id },
           },
-          userId: req.user.id,
         },
       });
+    const existingMovieInWatchlist = await prisma.watchlist.findFirst({
+      where: {
+        movieItems: {
+          some: {
+            movieId,
+          },
+        },
+      },
+    });
+    if (existingMovieInWatchlist)
+      return res
+        .status(409)
+        .json({ message: `Watchlist Already Contains Movie: ID ${movieId}` });
+
+    const existingMovie = await prisma.movie.findFirst({
+      where: {
+        imdbId: movieId,
+      },
+    });
+
+    if (!existingMovie) {
+      try {
+        const { data: movie } = await axios.get(
+          `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&i=${movieId}`
+        );
+        if (movie?.Response === "False") {
+          return res.status(404).json({ error: "Invalid Movie ID" });
+        }
+
+        await prisma.movie.create({
+          data: {
+            actors: movie.Actors,
+            awards: movie.Awards,
+            country: movie.Country,
+            genre: movie.Genre,
+            imdbId: movie.imdbID,
+            imdbRating: Number(movie.imdbRating),
+            language: movie.Language,
+            plot: movie.Plot,
+            poster: movie.Poster,
+            released: new Date(movie.Released),
+            runtime: Number(movie.Runtime.split(" ")[0]),
+            title: movie.Title,
+            type: movie.Type,
+            year: movie.Year,
+          },
+        });
+      } catch (err) {
+        return res.status(404).json({ error: "Error fetching the movie" });
+      }
+    }
 
     await prisma.watchlist.update({
       where: {
         id: watchlist.id,
       },
       data: {
-        watchListMovies: {
+        movieItems: {
           create: {
             movieId,
           },
-          connect: { id: watchlist.id },
         },
       },
     });
