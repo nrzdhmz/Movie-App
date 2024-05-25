b7372cf5d15ffe7fc38065d0282080c3cb2728fa
moaliyev <mehemmed05.aliyev@gmail.com>
2024-05-25 13:04:14 +0400

    bug-fix

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,13 +1,9 @@
 import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res) => {
-  const token = jwt.sign(
-    { userId },
-    "788532b2693e023ffd557ee0d66f7e98a435b823ed92589bfed6af29de5807c276438049fb1bd5e1b4e8b8fb5f81f9304985c79d80a3835fc22490b35b62beda",
-    {
-      expiresIn: "15d",
-    }
-  );
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
 
   res.cookie("jwt", token, {
     maxAge: 15 * 24 * 3600 * 1000, // Milli seconds
