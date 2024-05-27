9d3ede11d0117b803145f4532e74dfb8dcb56706
moaliyev <mehemmed05.aliyev@gmail.com>
2024-05-27 23:51:17 +0400

    feat: Endpoint added for removing movie from a watchlist

--- backend/controllers/watchlist.controller.js
+++ backend/controllers/watchlist.controller.js
@@ -122,3 +122,25 @@ export const updateMovieStatusController = async (req, res) => {
     return res.status(500).json({ error: "Server error" });
   }
 };
+
+// REMOVE MOVIE
+export const removeMovieController = async (req, res) => {
+  try {
+    const { movieId } = req.body;
+    const { id: userId } = req.user;
+    await prisma.watchlist.delete({
+      where: {
+        userId,
+        movieItems: {
+          some: {
+            movieId,
+          },
+        },
+      },
+    });
+    return res.status(200).json({ message: "Successfully deleted" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server error" });
+  }
+};
--- backend/routes/watchlist.routes.js
+++ backend/routes/watchlist.routes.js
@@ -4,6 +4,7 @@ import {
   addWatchlistController,
   getWatchlistController,
   updateMovieStatusController,
+  removeMovieController,
 } from "../controllers/watchlist.controller.js";
 
 const router = Router();
@@ -11,5 +12,6 @@ const router = Router();
 router.post("/", protectRoute, addWatchlistController);
 router.get("/", protectRoute, getWatchlistController);
 router.patch("/", protectRoute, updateMovieStatusController);
+router.delete("/", protectRoute, removeMovieController);
 
 export default router;
