3ceaed8f3c042b2665f686f3ea33cccacd70d115
moaliyev <mehemmed05.aliyev@gmail.com>
2024-05-28 00:00:04 +0400

    feat: Endpoint added for getting the list of followers of a user

--- backend/controllers/follow.controllers.js
+++ backend/controllers/follow.controllers.js
@@ -1,7 +1,7 @@
 import prisma from "../prismaClient";
 
 // Get following list of the user
-export const getFollowing = async (req, res) => {
+export const getFollowingController = async (req, res) => {
   try {
     const { id: userId } = req.user;
     const followingList = await prisma.follow.findMany({
@@ -18,6 +18,30 @@ export const getFollowing = async (req, res) => {
       },
     });
     return res.status(200).json({ followingList });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Server error" });
+  }
+};
+
+// Get followers list of the user
+export const getFollowerController = async (req, res) => {
+  try {
+    const { id: userId } = req.user;
+    const followerList = await prisma.follow.findMany({
+      where: {
+        followingId: userId,
+      },
+      include: {
+        follwerUser: {
+          select: {
+            _count: true,
+            username: true,
+          },
+        },
+      },
+    });
+    return res.status(200).json({ followerList });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "Server error" });
